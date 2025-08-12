39627fef08b4f05e5042e323f50a904312e2836c
matthew-pisano <matthewpisano14@gmail.com>
2025-08-11 21:27:06 -0400

    Update Hello World Program

--- modules/module_01/helloworld.asm
+++ modules/module_01/helloworld.asm
@@ -1,18 +1,19 @@
 # Module 01 Demo A
 # Helloworld test program
 
-.data
+.data                   # The static data section of the program
 
-msg:    .asciiz "Hello CSC250 class"
+msg:                    # A message to be printed out
+    .asciiz "Hello CSC250 class"
 
 
-.text
-.globl main
+.text                   # The executable text section of the program
 
-main:
-    li      $v0, 4      # syscall 4 (print_str)
-    la      $a0, msg    # argument: string
-    syscall             # print the string
+.globl main             # Make the main label globally accessible
+main:                   # The start of the main program
+    li      $v0, 4      # Load syscall code 4 (print_str) into $v0
+    la      $a0, msg    # Load the address of msg into $a0
+    syscall             # Execute the syscall to print the string
 
-    li      $v0, 10     # syscall 10 (exit)
-    syscall             # call operating system
+    li      $v0, 10     # Load syscall code 10 (exit) into $v0
+    syscall             # Execute the syscall to exit the program gracefully
